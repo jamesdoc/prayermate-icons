d97dd2726ab9ea18224b1d301a3d3f9fd0fda4b0
James Doc <hi@jamesdoc.com>
2023-08-09 12:53:42 +0100

    Asset update

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.on("afterBuild", () => {
     return esbuild.build({
       entryPoints: ["./src/assets/script.js"],
-      outdir: "dist/assets",
+      outdir: "docs/assets",
       bundle: true,
       minify: process.env.ELEVENTY_ENV === "production",
       sourcemap: process.env.ELEVENTY_ENV !== "production",
